d8e3f0dc0ef84a6b608e1e841fe2bbacd7ee3f4e
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-21 10:18:28 -0500

    Move is.leapYear to its own file
    Also, remove it from orfwxfunctions.R

---
+++ orfwx/is-leapyear.R
@@ -0,0 +1,4 @@
+is.leapYear=function(year){
+  #http://en.wikipedia.org/wiki/Leap_year
+  return(((year %% 4 == 0) & (year %% 100 != 0)) | (year %% 400 == 0))
+}
--- orfwxfunctions.R
+++ orfwxfunctions.R
@@ -1,8 +1,3 @@
-is.leapYear=function(year){
-  #http://en.wikipedia.org/wiki/Leap_year
-  return(((year %% 4 == 0) & (year %% 100 != 0)) | (year %% 400 == 0))
-}
-
 readWeatherData <- function() {
   # Read Norfolk airport weather data from CSV
   airport <- read.csv("NorfolkIntlAp.csv",
