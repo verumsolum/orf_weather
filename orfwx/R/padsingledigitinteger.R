411b3b38624068c669edd6898349f343a277d2ed
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-21 17:21:18 -0500

    Handle invalid values of theInteger passed to padSingleDigitInteger
    Also updates function's documentation

--- orfwx/R/padsingledigitinteger.R
+++ orfwx/R/padsingledigitinteger.R
@@ -3,22 +3,31 @@
 #' \code{padSingleDigitInteger} returns a two-character string, padding a
 #' single-digit integer with a leading zero, if required.
 #' 
-#' This function currently converts \code{theInteger} into an integer and
-#' returns that integer if greater than 10; otherwise, it adds a leading
-#' zero, intended to create a two-character string.
+#' This function converts \code{theInteger} into an integer and
+#' returns that integer if between 10 and 99 (inclusive); otherwise, 
+#' it adds a leading zero, to create a two-character string.
 #' 
-#' The input is not currently checked for all necessary correctness.
-#' For sensible results, \code{TheInteger} should be a positive integer,
-#' less than or equal to 99.
+#' Errors will be provided for negative integers and integers with
+#' more than two digits.
 #' 
-#' @param theInteger Any integer.
-#' @return Intended to return a two-character string
+#' @param theInteger Any integer between 0 and 99 (inclusive).
+#' @return Returns a two-character string.
 #' @examples
-#' padSingleDigitInteger(20)  # 20
+#' padSingleDigitInteger(20)  # "20"
 #' padSingleDigitInteger(9)  # "09"
 #' padSingleDigitInteger(0)  # "00"
 #' @export
 padSingleDigitInteger <- function(theInteger) {
-  if(as.numeric(theInteger) >= 10 ) return(abs(as.integer(theInteger)))  # What is good style for if in R
-  paste0("0", as.character(abs(as.integer(theInteger))))
+  # Fail if given invalid inputs
+  if(as.integer(theInteger) >= 100) stop("theInteger has more than 2 digits")
+  if(as.integer(theInteger) < 0) stop("theInteger is a negative integer")
+  
+  # If theInteger is valid, return it as a string
+  theStringVersion <- as.character(abs(as.integer(theInteger)))
+  if(as.integer(theInteger) >= 10 ) {
+    return(theStringVersion)
+  } else {
+    theStringVersion <- paste0("0", theStringVersion)
+    return(theStringVersion)
+  }
 }
