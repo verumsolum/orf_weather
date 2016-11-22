da7f1501aed44ab52b554c6b958d20baddfea9bb
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-22 09:39:21 -0500

    Copy readWeatherData function to file in data-raw
    (Still requires alteration to function.)

---
+++ orfwx/data-raw/readweatherdata.R
@@ -0,0 +1,25 @@
+readWeatherData <- function() {
+  # Leave this function in orfwxfunctions.R until package complete.
+  # Read Norfolk airport weather data from CSV
+  airport <- read.csv("orfwx/data-raw/NorfolkIntlAp.csv",
+                      colClasses = c("Date", "integer", "integer", "numeric", "factor", "factor"),
+                      na.strings = c("M", NULL))
+  overlappingDowntown <- read.csv("orfwx/data-raw/NorfolkWbCity.csv",
+                                  colClasses = c("Date", "integer", "integer", "numeric", "factor", "factor"),
+                                  na.strings = c("M", NULL))
+  # Remove duplicate dates (only use downtown before 1946-01-01)
+  downtown <- overlappingDowntown[which(overlappingDowntown$Date < as.Date("1946-01-01")), ]
+  bothStations <- rbind(airport, downtown)
+  bothStations <- arrange(bothStations, Date)
+  mutatedBothStations <- mutate(bothStations,
+                                Year = as.integer(strftime(Date, format="%Y")),
+                                Month = as.integer(strftime(Date, format="%m")),
+                                DayOfMonth = as.integer(strftime(Date, format="%d")),
+                                DayOfYear = as.integer(strftime(Date, format="%j")),
+                                MaxTemperature = as.integer(MaxTemperature),
+                                MinTemperature = as.integer(MinTemperature))
+  mutatedBothStations <- tbl_df(mutatedBothStations)
+  mutatedBothStations <- mutate(mutatedBothStations,
+                                leapYearAwareDayOfYear = ifelse(Month > 3 & !is.leapYear(Year), DayOfYear + 1, DayOfYear),
+                                temperatureSpread = as.integer(MaxTemperature - MinTemperature))
+}
