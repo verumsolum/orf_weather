e83c5a9a5c5522b30f5842f7bd7c11c50d11bf07
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-26 10:19:43 -0500

    Move plotWarmestTempOverHistory into its own file

---
+++ orfwx/R/plotwarmestmaxtempoverhistory.R
@@ -0,0 +1,43 @@
+plotWarmestMaxTempOverHistory <- function(plotDate = searchDate()) {
+  # Create a data frame with the weather for this day in history.
+  dayInHistory <- subset(readWeatherData(), format(Date, "%m%d") == format(plotDate, "%m%d"))
+  
+  # Make a data frame with only the year and maximum temperature (orftmax)
+  orfTMax <- data.frame("year" = as.integer(format(dayInHistory$Date, "%Y")),
+                        "highTemperature" = dayInHistory$MaxTemperature)
+  
+  # Manually add data for current year:
+  todayTMax <- data.frame("year" = format(plotDate, "%Y"),
+                          "highTemperature" = todaysHigh)  # Temp data frame with current observations
+  orfTMax <- rbind(orfTMax, todayTMax) # Merge with historical observations
+  
+  # Sort orfTMax by highTemperature
+  orfTMaxSorted <- arrange(orfTMax, desc(highTemperature))
+  if(orfTMaxSorted$highTemperature[10] <= todaysHigh) {
+    orfTMaxSorted <- filter(orfTMaxSorted, highTemperature >= orfTMaxSorted$highTemperature[10])
+  } else {
+    orfTMaxSorted <- filter(orfTMaxSorted, highTemperature >= todaysHigh)
+  }
+  # browser()
+  orfTMaxSorted <- arrange(orfTMaxSorted, highTemperature, as.integer(year))
+  
+  plotWithManyBars(orfTMaxSorted$highTemperature,
+                   orfTMaxSorted,
+                   paste("Warmest High Temperatures on", format(plotDate, "%b %d"), "in Norfolk Weather History"),
+                   paste("High temperature on", format(plotDate, "%b %d"), "(in °F)"),
+                   showAllLabels = TRUE
+  )
+  
+  # minor.tick(nx = 1,
+  #            ny = 5,
+  #            tick.ratio = 0.5)
+  # minor.tick(nx = 1,
+  #            ny = 10,
+  #            tick.ratio = 0.33)
+  minor.tick(nx = 1,
+             ny = 2,
+             tick.ratio = 0.67)
+  mtext('Since 1874')
+  #  mtext('Record warmest: __°F', line=-1)
+  #  mtext('Record coldest: __°F', line=-2)
+}
