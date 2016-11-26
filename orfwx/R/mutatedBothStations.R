394ebc48963ab24035b96c18d30b98c3f1ee3dbb
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-26 05:14:29 -0500

    First attempt at including data inside the package.

---
+++ orfwx/R/mutatedBothStations.R
@@ -0,0 +1,17 @@
+#' Historical weather observations from Norfolk, Virginia
+#' 
+#' A dataset containing observations of weather since 1874 from Norfolk, VA.
+#' Since 1946, those observations are taken at ORF, Norfolk International
+#' Airport. Prior to that, they were taken at the Weather Bureau offices in
+#' the city.
+#' 
+#' @format A data frame with 52,186 observations of 12 variables.
+#' \describe{
+#'   \item{Date}{the date of the observation}
+#'   \item{MaxTemperature}{the high temperature (in °F)}
+#'   \item{MinTemperature}{the low temperature (in °F)}
+#'   \item{AvgTemperature}{the average temperature (in °F)}
+#'   \item{Precipitation}{the day's precipitation (in inches)}
+#'   \item{Snowfall}{the day's snowfall (in inches)}
+#' }
+"mutatedBothStations"
