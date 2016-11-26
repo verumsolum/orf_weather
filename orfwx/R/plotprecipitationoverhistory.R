9e6ead03eace150f80edf14d3ef65d171108d91a
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-26 10:13:45 -0500

    Document and format plotPrecipitationOverHistory

--- orfwx/R/plotprecipitationoverhistory.R
+++ orfwx/R/plotprecipitationoverhistory.R
@@ -1,29 +1,51 @@
+#' Plot historical daily precipitation
+#' 
+#' \code{plotPrecipitationOverHistory} returns a barplot with the precipitation
+#' of one day each year.
+#' 
+#' This code makes my head hurt, because it was some of the earliest R code I
+#' wrote, when I was mostly in make-it-work mode, with huge gaps in my
+#' understanding of R (and of barplot particularly)
+#' 
+#' @param plotDate (optional) The date to be searched for, defaulting to the 
+#'   current date.
+#' @return Returns a barplot.
+#' @examples
+#' plotPrecipitationOverHistory(searchDate(11, 26))  # plot for November 26th
+#' @export
 plotPrecipitationOverHistory <- function(plotDate = searchDate()) {
   # Create a data frame with the weather for this day in history.
-  dayInHistory <- subset(readWeatherData(), format(Date, "%m%d") == format(plotDate, "%m%d"))
+  dayInHistory <- subset(mutatedBothStations, 
+                         format(Date, "%m%d") == format(plotDate, "%m%d"))
   
   # Make a data frame with only the year and maximum temperature (orftmax)
   orfPrcp <- data.frame("year" = as.integer(format(dayInHistory$Date, "%Y")),
-                        "precipitation" = as.numeric(as.character(dayInHistory$Precipitation)))
+                        "precipitation" = 
+                          as.numeric(as.character(dayInHistory$Precipitation)))
   
   # Manually add data for current year:
+  # todayPrcp is temp data frame with current observations
   todayPrcp <- data.frame("year" = format(plotDate, "%Y"),
-                          "precipitation" = todaysPrecipitation)  # Temp data frame with current observations
+                          "precipitation" = todaysPrecipitation)
   orfPrcp <- rbind(orfPrcp, todayPrcp) # Merge with historical observations
   
   # Remove years with text values for precipitation (missing and trace)
   orfPrcp <- na.omit(orfPrcp, orfPrcp$precipitation)
   
   # Remove years with no precipitation
-  orfPrcp <- orfPrcp[orfPrcp$precipitation > 0, ]  # Comment out to include 0.00" values
+  orfPrcp <- orfPrcp[orfPrcp$precipitation > 0, ]
   
   # Sort orfTMax by highTemperature
   orfPrcpSorted <- orfPrcp[order(orfPrcp$precipitation), ]
   
   plotWithManyBars(orfPrcpSorted$precipitation,
                    orfPrcpSorted,
-                   paste("Precipitation on", format(currentDate, "%b %d"), "in Norfolk Weather History"),
-                   paste("Precipitation on", format(currentDate, "%b %d"), "(in inches)"),
+                   paste("Precipitation on", 
+                         format(currentDate, "%b %d"), 
+                         "in Norfolk Weather History"),
+                   paste("Precipitation on", 
+                         format(currentDate, "%b %d"), 
+                         "(in inches)"),
                    TRUE
   )
   
@@ -37,6 +59,4 @@ plotPrecipitationOverHistory <- function(plotDate = searchDate()) {
   #            ny = 2,
   #            tick.ratio = 0.67)
   mtext('Since 1874')
-  #  mtext('Record warmest: __°F', line=-1)
-  #  mtext('Record coldest: __°F', line=-2)
 }
