b8a5f217c802524628eab724dcf3a04b5332dbca
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-26 07:26:12 -0500

    Document and format plotMaxTempOverHistory

--- orfwx/R/plotmaxtempoverhistory.R
+++ orfwx/R/plotmaxtempoverhistory.R
@@ -1,14 +1,31 @@
+#' Plot historical high tempertures
+#' 
+#' \code{plotMaxTempOverHistory} returns a barplot with the high temperatures
+#' of one day each year.
+#' 
+#' This code makes my head hurt, because it was some of the earliest R code I
+#' wrote, when I was mostly in make-it-work mode, with huge gaps in my
+#' understanding of R (and of barplot particularly)
+#' 
+#' @param plotDate (optional) The date to be searched for, defaulting to the 
+#'   current date.
+#' @return Returns a barplot.
+#' @examples
+#' plotMaxTempOverHistory(searchDate(11, 26))  # plot for November 26th
+#' @export
 plotMaxTempOverHistory <- function(plotDate = searchDate()) {
   # Create a data frame with the weather for this day in history.
-  dayInHistory <- subset(readWeatherData(), format(Date, "%m%d") == format(plotDate, "%m%d"))
+  dayInHistory <- subset(readWeatherData(), 
+                         format(Date, "%m%d") == format(plotDate, "%m%d"))
   
   # Make a data frame with only the year and maximum temperature (orftmax)
   orfTMax <- data.frame("year" = as.integer(format(dayInHistory$Date, "%Y")),
                         "highTemperature" = dayInHistory$MaxTemperature)
   
   # Manually add data for current year:
+  # todayTMax is temp data frame with current observations
   todayTMax <- data.frame("year" = format(plotDate, "%Y"),
-                          "highTemperature" = todaysHigh)  # Temp data frame with current observations
+                          "highTemperature" = todaysHigh)
   orfTMax <- rbind(orfTMax, todayTMax) # Merge with historical observations
   
   # Sort orfTMax by highTemperature
@@ -16,38 +33,13 @@ plotMaxTempOverHistory <- function(plotDate = searchDate()) {
   
   plotWithManyBars(orfTMaxSorted$highTemperature,
                    orfTMaxSorted,
-                   paste("High Temperatures on", format(plotDate, "%b %d"), "in Norfolk Weather History"),
-                   paste("High Temperature on", format(plotDate, "%b %d"), "(in °F)")
+                   paste("High Temperatures on", 
+                         format(plotDate, "%b %d"), 
+                         "in Norfolk Weather History"),
+                   paste("High Temperature on", 
+                         format(plotDate, "%b %d"), 
+                         "(in °F)")
   )
-  
-  ### START plotWithManyBars
-  
-  # # Convenience variables:
-  # recordLowTMax <- newOrfTMaxSorted$highTemperature[1]
-  # recordHighTMax <- newOrfTMaxSorted$highTemperature[length(newOrfTMaxSorted$highTemperature)]
-  # 
-  # # Graph orfTMaxSorted
-  # barplot(as.numeric(newOrfTMaxSorted$highTemperature),
-  #         cex.names = 0.6,  # Smaller text for year label
-  #         las = 2, # rotate labels perpendicular to axis
-  #         main = paste("High Temperatures on", format(plotDate, "%b %d"), "in Norfolk Weather History"),  # Title - CHANGE if updated to do other than currentDate
-  #         # xlab = "Year",  # Label for x axis
-  #         ylab = paste("High Temperature on", format(plotDate, "%b %d"), "(in °F)"),  # Label for y axis
-  #         ylim = c(floor((recordLowTMax - 1) / 5) * 5,
-  #                  ceiling((recordHighTMax + 1) /5) * 5),  # Limits of y axis  # TODO: Automatic limits
-  #         xpd = FALSE,  # Do not allow the bars to go outside the plot region (i.e., above/below ylim)
-  #         names.arg = ifelse(newOrfTMaxSorted$year == 2016 |
-  #                              newOrfTMaxSorted$highTemperature == recordLowTMax |
-  #                              newOrfTMaxSorted$highTemperature == recordHighTMax, 
-  #                            paste(newOrfTMaxSorted$year, "-", paste0(newOrfTMaxSorted$highTemperature, "°")),
-  #                            ""),  # Source of labels for x axis
-  #         col = ifelse(newOrfTMaxSorted$year == 2016, "mediumpurple", "steelblue1"),
-  #         border = "white")
-  # grid(nx = NA,
-  #      ny = NULL,
-  #      lty = "dotted")
-  
-  ### END plotWithManyBars
   
   # minor.tick(nx = 1,
   #            ny = 5,
@@ -59,6 +51,4 @@ plotMaxTempOverHistory <- function(plotDate = searchDate()) {
              ny = 2,
              tick.ratio = 0.67)
   mtext('Since 1874')
-  #  mtext('Record warmest: __°F', line=-1)
-  #  mtext('Record coldest: __°F', line=-2)
 }
