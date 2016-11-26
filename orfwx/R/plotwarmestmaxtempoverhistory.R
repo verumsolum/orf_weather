5d5c4e91a18ef632f601bb25f1cbc253f720a56f
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-26 10:27:38 -0500

    Document and edit plotWarmestMaxTempOverHistory

--- orfwx/R/plotwarmestmaxtempoverhistory.R
+++ orfwx/R/plotwarmestmaxtempoverhistory.R
@@ -1,20 +1,39 @@
+#' Plot warmest historical high tempertures on a single date
+#' 
+#' \code{plotWarmestMaxTempOverHistory} returns a barplot with the warmest 
+#' high temperatures of one day of the year.
+#' 
+#' This code makes my head hurt, because it was some of the earliest R code I
+#' wrote, when I was mostly in make-it-work mode, with huge gaps in my
+#' understanding of R (and of barplot particularly)
+#' 
+#' @param plotDate (optional) The date to be searched for, defaulting to the 
+#'   current date.
+#' @return Returns a barplot.
+#' @examples
+#' plotWarmestMaxTempOverHistory(searchDate(11, 26))  # plot for November 26th
+#' @export
 plotWarmestMaxTempOverHistory <- function(plotDate = searchDate()) {
   # Create a data frame with the weather for this day in history.
-  dayInHistory <- subset(readWeatherData(), format(Date, "%m%d") == format(plotDate, "%m%d"))
+  dayInHistory <- subset(mutatedBothStations, 
+                         format(Date, "%m%d") == format(plotDate, "%m%d"))
   
   # Make a data frame with only the year and maximum temperature (orftmax)
   orfTMax <- data.frame("year" = as.integer(format(dayInHistory$Date, "%Y")),
                         "highTemperature" = dayInHistory$MaxTemperature)
   
   # Manually add data for current year:
+  # todayTMax is data frame with current observations
   todayTMax <- data.frame("year" = format(plotDate, "%Y"),
-                          "highTemperature" = todaysHigh)  # Temp data frame with current observations
+                          "highTemperature" = todaysHigh)
   orfTMax <- rbind(orfTMax, todayTMax) # Merge with historical observations
   
   # Sort orfTMax by highTemperature
   orfTMaxSorted <- arrange(orfTMax, desc(highTemperature))
   if(orfTMaxSorted$highTemperature[10] <= todaysHigh) {
-    orfTMaxSorted <- filter(orfTMaxSorted, highTemperature >= orfTMaxSorted$highTemperature[10])
+    orfTMaxSorted <- filter(orfTMaxSorted, 
+                            highTemperature >= 
+                              orfTMaxSorted$highTemperature[10])
   } else {
     orfTMaxSorted <- filter(orfTMaxSorted, highTemperature >= todaysHigh)
   }
@@ -23,8 +42,12 @@ plotWarmestMaxTempOverHistory <- function(plotDate = searchDate()) {
   
   plotWithManyBars(orfTMaxSorted$highTemperature,
                    orfTMaxSorted,
-                   paste("Warmest High Temperatures on", format(plotDate, "%b %d"), "in Norfolk Weather History"),
-                   paste("High temperature on", format(plotDate, "%b %d"), "(in °F)"),
+                   paste("Warmest High Temperatures on", 
+                         format(plotDate, "%b %d"), 
+                         "in Norfolk Weather History"),
+                   paste("High temperature on", 
+                         format(plotDate, "%b %d"), 
+                         "(in °F)"),
                    showAllLabels = TRUE
   )
   
@@ -38,6 +61,4 @@ plotWarmestMaxTempOverHistory <- function(plotDate = searchDate()) {
              ny = 2,
              tick.ratio = 0.67)
   mtext('Since 1874')
-  #  mtext('Record warmest: __°F', line=-1)
-  #  mtext('Record coldest: __°F', line=-2)
 }
