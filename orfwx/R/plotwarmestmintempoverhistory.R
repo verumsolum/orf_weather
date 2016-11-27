7be2b36e26e7204937279d9b82c04454bb3fa9c6
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-27 06:05:35 -0500

    Move plotWarmestMinTempOverHistory to its own file

--- orfwx/R/plotwarmestmintempoverhistory.R
+++ orfwx/R/plotwarmestmintempoverhistory.R
@@ -1,4 +1,4 @@
-plotCoolestMinTempOverHistory <- function(plotDate = searchDate()) {
+plotWarmestMinTempOverHistory <- function(plotDate = searchDate()) {
   # Create a data frame with the weather for this day in history.
   dayInHistory <- subset(readWeatherData(), format(Date, "%m%d") == format(plotDate, "%m%d"))
   
@@ -7,23 +7,23 @@ plotCoolestMinTempOverHistory <- function(plotDate = searchDate()) {
                         "lowTemperature" = dayInHistory$MinTemperature)
   
   # Manually add data for current year:
-  todayTMin <- data.frame("year" = format(plotDate, "%Y"),
+  todayTMin <- data.frame("year" = as.integer(format(plotDate, "%Y")),
                           "lowTemperature" = todaysLow)  # Temp data frame with current observations
   orfTMin <- rbind(orfTMin, todayTMin) # Merge with historical observations
   
   # Sort orfTMin by lowTemperature
-  orfTMinSorted <- arrange(orfTMin, lowTemperature)
-  if(orfTMinSorted$lowTemperature[10] >= todaysLow) {
-    orfTMinSorted <- filter(orfTMinSorted, lowTemperature <= orfTMinSorted$lowTemperature[10])
+  orfTMinSorted <- arrange(orfTMin, desc(lowTemperature))
+  if(orfTMinSorted$lowTemperature[10] <= todaysLow) {
+    orfTMinSorted <- filter(orfTMinSorted, lowTemperature >= orfTMinSorted$lowTemperature[10])
   } else {
-    orfTMinSorted <- filter(orfTMinSorted, lowTemperature <= todaysLow)
+    orfTMinSorted <- filter(orfTMinSorted, lowTemperature >= todaysLow)
   }
   # browser()
-  orfTMinSorted <- arrange(orfTMinSorted, lowTemperature)
+  orfTMinSorted <- arrange(orfTMinSorted, lowTemperature, as.integer(year))
   
   plotWithManyBars(orfTMinSorted$lowTemperature,
                    orfTMinSorted,
-                   paste("Coolest Low Temperatures on", format(plotDate, "%b %d"), "in Norfolk Weather History"),
+                   paste("Warmest Low Temperatures on", format(plotDate, "%b %d"), "in Norfolk Weather History"),
                    paste("Low temperature on", format(plotDate, "%b %d"), "(in °F)"),
                    showAllLabels = TRUE
   )
