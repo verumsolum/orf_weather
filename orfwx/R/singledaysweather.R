d261ad199f4840e4a160a7974b445981e854d496
Kristin Rollins <dev@kristin.verumsolum.com>
2016-11-29 18:59:31 -0500

    Use hardcoded space in precipitation and snowfall

--- orfwx/R/singledaysweather.R
+++ orfwx/R/singledaysweather.R
@@ -26,8 +26,8 @@ singleDaysWeather <- function(highTemperature = NA,
   highTemperature <- as.integer(highTemperature)
   lowTemperature <- as.integer(lowTemperature)
   averageTemperature <- round(averageTemperature, digits = 1)
-  precipitation <- as.character(round(precipitation, digits = 2))
-  snowfall <- as.character(round(snowfall, digits = 1))
+  precipitation <- paste0(" ", as.character(round(precipitation, digits = 2)))
+  snowfall <- paste0(" ", as.character(round(snowfall, digits = 1)))
   
   # Create a data frame from the input variables
   sdw <- data.frame(highTemperature, 
