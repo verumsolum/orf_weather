9eb5ef7a07daf7f0b3a59ac11f6cb40d68b92893
Kristin Rollins <dev@kristin.verumsolum.com>
2016-12-08 09:29:56 -0500

    Adapt plotCoolestMaxTempOverHistory to remove dependency on environment

--- orfwx/R/plotcoolestmaxtempoverhistory.R
+++ orfwx/R/plotcoolestmaxtempoverhistory.R
@@ -9,11 +9,31 @@
 #' 
 #' @param plotDate (optional) The date to be searched for, defaulting to the 
 #'   current date.
+#' @param daysWeather (optional) The weather for a date not yet included in
+#'   the \code{mutatedBothStations} dataset, usually passed by the
+#'   \code{singleDaysWeather} function.
 #' @return Returns a barplot.
 #' @examples
 #' plotCoolestMaxTempOverHistory(searchDate(11, 26))  # plot for November 26th
 #' @export
-plotCoolestMaxTempOverHistory <- function(plotDate = searchDate()) {
+plotCoolestMaxTempOverHistory <- function(plotDate = searchDate(),
+                                          daysWeather = NULL) {
+  # Ensure that daysWeather is correct
+  if (!is.null(daysWeather)) {
+    # If daysWeather is set, 
+    # ensure that the date value from daysWeather matches
+    if (format(daysWeather$Date, "%m%d") != format(plotDate, "%m%d")) {
+      # If days don't match…
+      stop("plotDate and daysWeather$Date do not match")
+    } else {
+      # If they match, ensure that plotDate uses the same year as
+      # daysWeather$Date
+      plotDate <- as.Date(paste0(format(daysWeather$Date, "%Y"),
+                                 "-",
+                                 format(plotDate, "%m-%d")))
+    }
+  }
+  
   # Create a data frame with the weather for this day in history.
   dayInHistory <- subset(mutatedBothStations, 
                          format(Date, "%m%d") == format(plotDate, "%m%d"))
@@ -22,12 +42,16 @@ plotCoolestMaxTempOverHistory <- function(plotDate = searchDate()) {
   orfTMax <- data.frame("year" = as.integer(format(dayInHistory$Date, "%Y")),
                         "highTemperature" = dayInHistory$MaxTemperature)
   
-  # Manually add data for current year:
-  # todayTMax is temp data frame with current observations
-  todayTMax <- data.frame("year" = format(plotDate, "%Y"),
-                          "highTemperature" = todaysHigh)  
-  orfTMax <- rbind(orfTMax, todayTMax) # Merge with historical observations
-  
+  # If daysWeather is not NULL, add data for current year:
+  if (!is.null(daysWeather)) {
+    daysWeatherYear <- format(daysWeather$Date, "%Y")
+    # todayTMax is temp data frame with current observations
+    todayTMax <- data.frame("year" = format(plotDate, "%Y"),
+                            "highTemperature" = daysWeather$MaxTemperature)
+    orfTMax <- rbind(orfTMax, todayTMax) # Merge with historical observations
+  } else {
+    daysWeatherYear <- format(Sys.Date(), "%Y")
+  }
   # Sort orfTMax by highTemperature
   orfTMaxSorted <- arrange(orfTMax, highTemperature)
   if(orfTMaxSorted$highTemperature[10] >= todaysHigh) {
@@ -48,7 +72,8 @@ plotCoolestMaxTempOverHistory <- function(plotDate = searchDate()) {
                    paste("High temperature on", 
                          format(plotDate, "%b %d"), 
                          "(in °F)"),
-                   showAllLabels = TRUE
+                   showAllLabels = TRUE,
+                   highlightYear = daysWeatherYear
   )
   
   # minor.tick(nx = 1,
