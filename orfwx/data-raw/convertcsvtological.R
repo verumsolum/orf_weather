f59c38cb899843cc895d651427acdadf013a515d
Kristin Rollins <dev@kristin.verumsolum.com>
2016-12-13 10:39:07 -0500

    Remove unused convertCsvToLogical

--- orfwx/data-raw/convertcsvtological.R
+++
@@ -1,7 +0,0 @@
-convertCsvToLogical <- function(reportedValue) {
-  ifelse(reportedValue == "0.00",
-         return(FALSE),  # if "0.00"
-         ifelse(reportedValue == "M" | is.null(reportedValue),
-                return(NA),  # if "M" or NULL
-                return(TRUE)))  # otherwise
-}
