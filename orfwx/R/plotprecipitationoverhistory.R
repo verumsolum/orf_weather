5c1c9e791ab9059246b6d1b17aa297933212804e
Kristin Rollins <dev@kristin.verumsolum.com>
2016-12-14 05:06:57 -0500

    Precipitation -> CsvPrecipitation in another place in plotPrecipitationOverHistory

--- orfwx/R/plotprecipitationoverhistory.R
+++ orfwx/R/plotprecipitationoverhistory.R
@@ -53,7 +53,8 @@ plotPrecipitationOverHistory <- function(plotDate = searchDate(),
   # Make a data frame with only the year and maximum temperature (orftmax)
   orfPrcp <- data.frame("year" = as.integer(format(dayInHistory$Date, "%Y")),
                         "precipitation" = 
-                          as.numeric(as.character(dayInHistory$Precipitation)))
+                          as.numeric(as.character(
+                            dayInHistory$CsvPrecipitation)))
   
   # If daysWeather is not NULL, add data for current year:
   if (!is.null(daysWeather)) {
