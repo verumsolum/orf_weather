68c07cdd8c8808175ff4d323a1dc70b7918fe15c
Kristin Rollins <dev@kristin.verumsolum.com>
2016-12-23 03:38:36 -0500

    Create ensureDirectoryExists and update getUpdatedCsv to write file

--- orfwx/R/getupdatedcsv.R
+++ orfwx/R/getupdatedcsv.R
@@ -3,7 +3,7 @@
 #' \code{getUpdatedCsv} takes two data frames and combines them.
 #' 
 #' This is a convenience wrapper that uses \code{httr} to download an
-#' \code{update.csv} file
+#' \code{updates.csv} file
 #' 
 #' @return Downloads a file to the working directory.
 #' @examples
@@ -13,5 +13,28 @@ getUpdatedCsv <- function(){
   gucURL <- "https://github.com/verumsolum/orf_weather/raw/wxupdate/orfwx/data-raw/updates.csv"
   hResponse <- httr::GET(gucURL)
   fileContents <- httr::content(hResponse, "raw")
-  # writeBin(fileContents, filename)  # TODO: Where should we write the file?
+  fLocation <- "~/.orfwx/updates.csv"
+  bfTimestamp <- strftime(Sys.time(), "%Y%m%d%H%M%S")
+  bfName <- paste0("~/.orfwx/updates.", bfTimestamp, ".backup.csv")
+  # If the file exists, rename it (as a backup), and then overwrite with 
+  # the new file.
+  if (file.exists(fLocation)) {
+    file.rename(fLocation, bfName)
+    if (!file.exists(bfName)) {
+      stop("Backup file does not appear to have been created")
+    }
+  }
+  # Because we have renamed the existing file (if it existed), 
+  # it should not exist at the name we want to use. But just in case...
+  if (!file.exists(fLocation)) {
+    # First, make sure the directory exists, then write the file.
+    if (!ensureDirectoryExists(createIfMissing = TRUE)) {
+      stop("Directory does not exist and apprently could not be created.")
+    }
+    writeBin(fileContents, fLocation)
+  } else {
+    stop(paste("Update not written to file: A file at",
+               fLocation,
+               "still exists after renaming."))
+  }
 }
