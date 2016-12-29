c80b085c0b17abdc49dd256a9c7a89478cad8300
Kristin Rollins <dev@kristin.verumsolum.com>
2016-12-29 18:54:54 -0500

    Fix line length and add consecutiveSnowfall variable

--- orfwx/R/computeconsecutiveprecipvariables.R
+++ orfwx/R/computeconsecutiveprecipvariables.R
@@ -12,14 +12,32 @@
 #' computeConsecutivePrecipVariables(convertCsvToNumericAndLogical(computeExtraDateVariables(airportData)))
 #' @export
 computeConsecutivePrecipVariables <- function(originalFrame) {
+  # Set up variables for consecutive days...
   originalFrame[["consecutivePrecipitation"]] <- 0
+  originalFrame[["consecutiveSnowfall"]] <-0
+
   for(i in 1:nrow(originalFrame)) {
-    if(!is.na(originalFrame[["WithPrecipitation"]][i]) & originalFrame[["WithPrecipitation"]][i] == TRUE) {
+    # For each row, first calculate for precipitation.
+    if(!is.na(originalFrame[["WithPrecipitation"]][i]) & 
+       originalFrame[["WithPrecipitation"]][i] == TRUE) {
       # If WithPrecipitation == TRUE, then in most cases...
       if(i != 1) {
-        originalFrame[["consecutivePrecipitation"]][i] = originalFrame[["consecutivePrecipitation"]][i - 1] + 1
+        originalFrame[["consecutivePrecipitation"]][i] = 
+          originalFrame[["consecutivePrecipitation"]][i - 1] + 1
       } else {
         originalFrame[["consecutivePrecipitation"]][i] = 1
+      }
+    }
+    
+    # ...then calculate for snowfall.
+    if(!is.na(originalFrame[["WithSnowfall"]][i]) &
+       originalFrame[["WithSnowfall"]][i] == TRUE) {
+      # If WithSnowfall == TRUE, then in most cases...
+      if(i != 1) {
+        originalFrame[["consecutiveSnowfall"]][i] =
+          originalFrame[["consecutiveSnowfall"]][i - 1] + 1
+      } else {
+        originalFrame[["consecutiveSnowfall"]][i] = 1
       }
     }
   }
