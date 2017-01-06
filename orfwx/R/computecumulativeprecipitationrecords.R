6fbcdcdc567d0d1d3da2de785609fede5c83fa89
Kristin Rollins <dev@kristin.verumsolum.com>
2017-01-06 13:17:21 -0500

    Start figuring out the most recent year for each record in computeCumulativePrecipitationRecords

--- orfwx/R/computecumulativeprecipitationrecords.R
+++ orfwx/R/computecumulativeprecipitationrecords.R
@@ -23,12 +23,18 @@ computeCumulativePrecipitationRecords <-
       ccprMonth <- as.integer(format(orfwx::yesterdate(), "%m"))
     }
     
-    recordsFrame <- orfwx::computeExtraDateVariables(originalFrame) %>% 
+    originalFrame <- orfwx::computeExtraDateVariables(originalFrame) %>% 
       dplyr::select(-DayOfYear) %>% 
-      dplyr::filter(Month == ccprMonth) %>% 
-      dplyr::group_by(Month, DayOfMonth) %>% 
+      dplyr::filter(Month == ccprMonth)
+    recordsFrame <- dplyr::group_by(originalFrame, Month, DayOfMonth) %>% 
       dplyr::summarise(maxMTDPrecip = max(MTDPrecip),
                        minMTDPrecip = min(MTDPrecip)) %>% 
       as.data.frame()
+    
+    # Determine the most recent year for the records.
+    yearsFrame <- dplyr::select(recordsFrame, Month, DayOfMonth)
+    # CURRENTLY: yearsFrame only contains the Month and DayOfMonth for the
+    #            appropriate month.
+    
     return(recordsFrame)
 }
