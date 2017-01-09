9dcb1099972a22fe830ec4bb055defb9df350a5d
Kristin Rollins <dev@kristin.verumsolum.com>
2017-01-09 06:16:35 -0500

    Add legend to plot

--- orfwx/R/plotmtdprecipitation.R
+++ orfwx/R/plotmtdprecipitation.R
@@ -14,25 +14,29 @@
 #' @export
 plotMTDPrecipitation <- function() {
   # DRAFT - not yet suitable for inclusion in package
+  col2legend <- c("Maximum" = "firebrick", "2017" = "black", "Minimum" = "blue")
   ggplot2::ggplot(orfwx::computeCumulativePrecipitationRecords(
                     showYear = TRUE),
-                  ggplot2::aes(DayOfMonth, maxMTDPrecip)) +
-    ggplot2::geom_point(color = "firebrick") + 
-    ggplot2::geom_line(color = "firebrick") + 
-    ggplot2::geom_text(ggplot2::aes(label = maxMTDYear), 
+                  ggplot2::aes(DayOfMonth, maxMTDPrecip, color = "Maximum")) +
+    ggplot2::geom_point(ggplot2::aes(color = "Maximum")) + 
+    ggplot2::geom_line(ggplot2::aes(color = "Maximum")) + 
+    ggplot2::geom_text(ggplot2::aes(label = maxMTDYear, color = "Maximum"), 
                        nudge_y = 0.5, 
                        angle = 90, 
-                       size = 3) +
+                       size = 3,
+                       color = "black") +
     ggplot2::ggtitle("January month-to-date precipitation") + 
     ggplot2::theme(plot.title = ggplot2::element_text(face = "bold",
                                                       family = "Optima")) + 
     ggplot2::labs(x = "Day of January", y = "Precipitation (in inches)") + 
-    ggplot2::geom_point(ggplot2::aes(y = MTD), color = "black") + 
-    ggplot2::geom_line(ggplot2::aes(y = MTD), color = "black") + 
-    ggplot2::geom_point(ggplot2::aes(y = minMTDPrecip), color = "blue") + 
-    ggplot2::geom_line(ggplot2::aes(y = minMTDPrecip), color = "blue") + 
-    ggplot2::geom_text(ggplot2::aes(y = minMTDPrecip, label = minMTDYear), 
+    ggplot2::geom_point(ggplot2::aes(y = MTD, color = "2017")) + 
+    ggplot2::geom_line(ggplot2::aes(y = MTD, color = "2017")) + 
+    ggplot2::geom_point(ggplot2::aes(y = minMTDPrecip, color = "Minimum")) + 
+    ggplot2::geom_line(ggplot2::aes(y = minMTDPrecip, color = "Minimum")) + 
+    ggplot2::geom_text(ggplot2::aes(y = minMTDPrecip, label = minMTDYear, color = "Minimum"), 
                        nudge_y = -0.5, 
                        angle = 90, 
-                       size = 3)
+                       size = 3,
+                       color = "black") +
+    ggplot2::scale_color_manual(name = "Legend", values = col2legend)
   }
