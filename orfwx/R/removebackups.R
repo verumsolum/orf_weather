a9f4ba147030ccd98fe11cc6258537fe06689adb
Kristin Rollins <dev@kristin.verumsolum.com>
2017-02-02 12:28:39 -0500

    Remove old commented-out lines from copied function.

--- orfwx/R/removebackups.R
+++ orfwx/R/removebackups.R
@@ -48,36 +48,4 @@ removeBackups <- function(leaveOne = FALSE) {
       .[-1, ]  # Exclude the most recent file
     file.remove(rbInfo[["filename"]])
   }
-  
-  # # If the file exists, rename it (as a backup), and then overwrite with 
-  # # the new file.
-  # if (file.exists(fLocation)) {
-  #   file.rename(fLocation, bfName)
-  #   if (!file.exists(bfName)) {
-  #     stop("Backup file does not appear to have been created")
-  #   }
-  # }
-  # # Because we have renamed the existing file (if it existed), 
-  # # it should not exist at the name we want to use. But just in case...
-  # if (!file.exists(fLocation)) {
-  #   # First, make sure the directory exists, then write the file.
-  #   if (!ensureDirectoryExists(createIfMissing = TRUE)) {
-  #     stop("Directory does not exist and apprently could not be created.")
-  #   }
-  #   utils::download.file(gucURL, fLocation)
-  #   # Notify the user whether or not the update is up-to-date or not
-  #   lastUpdated <- orfwx::findMostRecentDate()
-  #   if(lastUpdated == orfwx::yesterdate()) {
-  #     message(paste0("UP TO DATE! Updated through yesterday (", 
-  #                    lastUpdated, 
-  #                    ")"))
-  #   } else {
-  #     message(paste("OUT OF DATE\n-----------\nMost recent date included:",
-  #                   lastUpdated))
-  #   }
-  # } else {
-  #   stop(paste("Update not written to file: A file at",
-  #              fLocation,
-  #              "still exists after renaming."))
-  # }
 }
