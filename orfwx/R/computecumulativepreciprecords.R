633eda19b7d72a6416429fa436475d2732cfe05c
Kristin Rollins <dev@kristin.verumsolum.com>
2017-02-08 18:50:08 -0500

    Pipe originalFrame in, in prep for adding other steps

--- orfwx/R/computecumulativepreciprecords.R
+++ orfwx/R/computecumulativepreciprecords.R
@@ -66,7 +66,8 @@ computeCumulativePrecipRecords <-
     originalFrame <- orfwx::computeExtraDateVariables(originalFrame) %>% 
       dplyr::select(-DayOfYear) %>% 
       dplyr::filter(Month == ccprMonth)
-    recordsFrame <- dplyr::group_by(originalFrame, Month, DayOfMonth) %>% 
+    recordsFrame <- originalFrame %>%
+      dplyr::group_by(Month, DayOfMonth) %>% 
       dplyr::summarise(maxMTDPrecip = max(MTDPrecip),
                        minMTDPrecip = min(MTDPrecip),
                        maxYTDPrecip = max(YTDPrecip),
