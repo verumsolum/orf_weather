cf2b18d6db47548bc6de4e2a746208b882816d50
Kristin Rollins <dev@kristin.verumsolum.com>
2017-02-15 05:20:41 -0500

    Start dayEachYear with documentation

---
+++ orfwx/R/dayeachyear.R
@@ -0,0 +1,17 @@
+#' Find the weather for a give day each year.
+#' 
+#' `dayEachYear` takes a data frame with weather information and returns only
+#' the weather for one particular day of each year.
+#' 
+#' Details section to be written.
+#' 
+#' @param deyMonth An integer representing the month of the year (1=January, 
+#'   12=December) for which the date we are interested in.
+#' @param deyDayOfMonth An integer representing the day of the month (1-31) for
+#'   the date we are interested in.
+#' @return A data frame is returned,
+#' @examples
+#' dayEachYear(2, 15)  # Weather for February 15th
+#' @export
+dayEachYear <- function(deyMonth, deyDayOfMonth){
+}
